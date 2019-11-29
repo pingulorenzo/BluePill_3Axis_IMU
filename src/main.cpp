97e61039a7a989e66be4d5a83591eefb07b21597
Lorenzo D'Agostino <lorenzodagostino@gmail.com>
2019-11-29 19:59:27 +0100

    Code commented and cleaned | Version 1.0

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,23 @@
 TwoWire WIRE2 (2, I2C_FAST_MODE);
 #define Wire WIRE2
 
-#define TRIES 500
+#define N_SAMPLES 200
+#define LED_BLUE PC15
+#define LED_RED PC14
+#define SCALE_FACTOR 8192.0
 
 const int MPU = 0x68;  // I2C address of the MPU-6050
 int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
 int32_t AcXOff, AcYOff, AcZOff;
+float FAcX, FAcY, FAcZ;
 
+// Calibrate by reading, averaging and calculating offsets
 void calibrateMPU(){
-    for (int i = 0; i < TRIES; i++){
+    // LED on to signal start of calibration
+    digitalWrite(LED_BLUE, HIGH);
+
+    // Cycle for N_SAMPLES times and accumulate
+    for (int i = 0; i < N_SAMPLES; i++){
         Wire.beginTransmission(MPU);
         Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
         Wire.endTransmission(false);
@@ -25,50 +34,75 @@ void calibrateMPU(){
         delay(50);
     }
 
-    AcXOff = AcXOff / TRIES;
-    AcYOff = AcYOff / TRIES;
-    AcZOff = AcZOff / TRIES - 8192;
+    // Compute average
+    AcXOff = AcXOff / N_SAMPLES;
+    AcYOff = AcYOff / N_SAMPLES;
+    AcZOff = AcZOff / N_SAMPLES - 8192;
+
+    // LED off to signal end of calibration
+    digitalWrite(LED_BLUE, LOW);
 }
 
 void setup(){
+    // Start LED Pins
+    pinMode(LED_BLUE, OUTPUT);
+    pinMode(LED_RED, OUTPUT);
+
+    // Start I2C Communication
     Wire.begin();
     Wire.beginTransmission(MPU);
     Wire.write(0x6B);  // PWR_MGMT_1 register
     Wire.write(0);     // set to zero (wakes up the MPU-6050)
     Wire.endTransmission(false);
+
+    // Select Accelerometer mode (4g)
     Wire.beginTransmission(MPU);
     Wire.write(0x1C);  // AccSel register
     Wire.write(0x08);  // set to 00001000 to AccSel = 1
     Wire.endTransmission(true);
+
+    // Start Serial Port 1 (A9, A10)
     Serial1.begin(9600);
+
+    // Start Calibration
     calibrateMPU();
 }
 void loop(){
+    // Write starting register
     Wire.beginTransmission(MPU);
-    Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+    Wire.write(0x3B);  // 0x3B (ACCEL_XOUT_H)
     Wire.endTransmission(false);
 
-    Wire.requestFrom(MPU, 6);  // request a total of 6 registers
+    // Request data from MPU
+    Wire.requestFrom(MPU, 6);  // request 6 registers
     AcX = Wire.read() << 8 | Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
     AcY = Wire.read() << 8 | Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
     AcZ = Wire.read() << 8 | Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
 
+    // Subtract Offsets
     AcX -= ((int16_t)AcXOff);
     AcY -= ((int16_t)AcYOff);
     AcZ -= ((int16_t)AcZOff);
 
-    float FAcX, FAcY, FAcZ;
+    // Divide by SCALE_FACTOR for the selected mode (8192 for 4g sensitivity)
+    FAcX = AcX / SCALE_FACTOR;
+    FAcY = AcY / SCALE_FACTOR;
+    FAcZ = AcZ / SCALE_FACTOR;
 
-    FAcX = AcX / 8192.0;
-    FAcY = AcY / 8192.0;
-    FAcZ = AcZ / 8192.0;
-
-    Serial1.print("Accelerometer: ");
-    Serial1.print("X = "); Serial1.print(FAcX);
-    Serial1.print(" | Y = "); Serial1.print(FAcY);
-    Serial1.print(" | Z = "); Serial1.println(FAcZ);
+    // Cast to int16 the values*100 to send on Serial Port
+    Serial1.write(int16_t(FAcX*100));
+    Serial1.write(int16_t(FAcY*100));
+    Serial1.write(int16_t(FAcZ*100));
+    
+    // DEBUG SECTION
+    // Serial1.print("Accelerometer: ");
+    // Serial1.print("X = "); Serial1.print(int16_t(FAcX*100));
+    // Serial1.print(" | Y = "); Serial1.print(int16_t(FAcY*100));
+    // Serial1.print(" | Z = "); Serial1.println(int16_t(FAcZ*100));
     // Serial1.print("OffX = "); Serial1.print(AcXOff);
     // Serial1.print(" | OffY = "); Serial1.print(AcYOff);
     // Serial1.print(" | OffZ = "); Serial1.println(AcZOff);
-    delay(333);
+    
+    // Delay 100 ms
+    delay(100);
 }
