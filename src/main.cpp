dd25404668533c13753761f07deabddd64aafb3f
Lorenzo D'Agostino <lorenzodagostino@gmail.com>
2020-01-17 05:55:16 +0100

    Added serial protocol

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 
-// MPU-6050 Short Example Sketch
 #include<Wire.h>
 
 TwoWire WIRE2 (2, I2C_FAST_MODE);
@@ -19,7 +18,7 @@ float FAcX, FAcY, FAcZ;
 // Calibrate by reading, averaging and calculating offsets
 void calibrateMPU(){
     // LED on to signal start of calibration
-    digitalWrite(LED_BLUE, HIGH);
+    digitalWrite(LED_RED, HIGH);
 
     // Cycle for N_SAMPLES times and accumulate
     for (int i = 0; i < N_SAMPLES; i++){
@@ -40,7 +39,7 @@ void calibrateMPU(){
     AcZOff = AcZOff / N_SAMPLES - 8192;
 
     // LED off to signal end of calibration
-    digitalWrite(LED_BLUE, LOW);
+    digitalWrite(LED_RED, LOW);
 }
 
 void setup(){
@@ -89,19 +88,40 @@ void loop(){
     FAcY = AcY / SCALE_FACTOR;
     FAcZ = AcZ / SCALE_FACTOR;
 
-    // Cast to int16 the values*100 to send on Serial Port
-    Serial1.write(int16_t(FAcX*100));
-    Serial1.write(int16_t(FAcY*100));
-    Serial1.write(int16_t(FAcZ*100));
+    // check for one byte and notify by turning on LED
+    digitalWrite(LED_BLUE, HIGH);
+    if (Serial1.available() > 0){
+        Serial1.read();        
+
+        // Cast to uint16 the values*100 to send on Serial Port
+        uint16_t serial_ax, serial_ay, serial_az;
+        serial_ax = uint16_t(FAcX*100);
+        serial_ay = uint16_t(FAcY*100);
+        serial_az = uint16_t(FAcZ*100);
+
+        // Split High and Low
+        uint8_t serial_hax, serial_lax, serial_hay, serial_lay, serial_haz, serial_laz;
+        serial_hax = serial_ax >> 8;
+        serial_lax = serial_ax & 0x00FF;
+        serial_hay = serial_ay >> 8;
+        serial_lay = serial_ay & 0x00FF;
+        serial_haz = serial_az >> 8;
+        serial_laz = serial_az & 0x00FF;
+
+        // Send on serial
+        Serial1.write(serial_hax);
+        Serial1.write(serial_lax);
+        Serial1.write(serial_hay);
+        Serial1.write(serial_lay);
+        Serial1.write(serial_haz);
+        Serial1.write(serial_laz);
+
+        // Notify end of Serial Write by turning LED off
+        digitalWrite(LED_BLUE, LOW);
+    }
     
     // DEBUG SECTION
-    // Serial1.print("Accelerometer: ");
-    // Serial1.print("X = "); Serial1.print(int16_t(FAcX*100));
-    // Serial1.print(" | Y = "); Serial1.print(int16_t(FAcY*100));
-    // Serial1.print(" | Z = "); Serial1.println(int16_t(FAcZ*100));
-    // Serial1.print("OffX = "); Serial1.print(AcXOff);
-    // Serial1.print(" | OffY = "); Serial1.print(AcYOff);
-    // Serial1.print(" | OffZ = "); Serial1.println(AcZOff);
+    // Serial1.println("DEBUG");
     
     // Delay 100 ms
     delay(100);
